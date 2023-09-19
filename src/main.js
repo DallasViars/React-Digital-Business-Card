94c9017157cd0c1bc0e1b555ca63d65233c9b1d3
Dallas Viars <97245426+DallasViars@users.noreply.github.com>
2023-09-18 21:22:13 -0400

    Updated file name

--- src/main.js
+++ src/main.js
@@ -5,15 +5,9 @@ import "./index.css"
 <ass></ass>
 
 const root = document.querySelector("#root");
-// const toggleBtn = document.querySelector(".btn-toggle");
 
 ReactDOM.createRoot(root).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 )
-
-// console.log(root)
-// toggleBtn.addEventListener("click", (e) => {
-//   root.classList.toggle("darkmode")
-// });
