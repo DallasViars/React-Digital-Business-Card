1dfb5dd2f0f17eefb78c23ecf9cf741bf4b096b7
Dallas Viars <97245426+DallasViars@users.noreply.github.com>
2023-09-18 21:19:13 -0400

    Added import React in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import Header from "./assets/components/Header.jsx";
 import MainContent from "./assets/components/MainContent.jsx";
 import Footer from "./assets/components/Footer.jsx";
